519aeeb7a7536af93b0ae94324158e529a14ed80
Keletso Nkgadima <KeletsoNkgadima6@gmail.com>
2026-01-15 14:20:02 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp48
+{
+    internal class Calculator
+    {
+        public string Name { get; set; }
+
+        public Calculator(string name)
+        {
+            Name = name;
+        }
+
+        //you want the method to add double 
+        //addition mehtod
+        // you want to declare a method tha returns you must name it static to avoid errors in the actual code
+        public static double add(double A, double B)
+        {
+            //the method will e dark if you don't call it
+            //for the computher to be able to write you tell it what todo to be able to add
+            double additon = A + B;
+            return additon;
+        }
+        //subtraction method
+        public static double subtract(double A, double B)
+        {
+
+            //tell the mehtod to subtract 
+
+            double subtraction = A - B;
+            return subtraction;
+        }
+       public static double multiply(double A, double B)
+        {
+
+            double multiply = A * B;
+            return multiply;
+
+        }
+        public static double devide(double A, double B)
+        {
+            double devide  = A / B;
+            return devide;
+
+        }
+        public static double iseven(double A)
+        {
+          if ( A % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                    Console.WriteLine("The number is odd");
+            }
+            return A;
+        }
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator("Pheladi");
+
+            Console.WriteLine("Enter the first number");
+            double Num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            double Num2 = Convert.ToDouble(Console.ReadLine());
+
+            //We have to ask the user if they want to add,subract, multiply or devide
+            Console.WriteLine();
+            Console.WriteLine("How do you want to calculate your numbers");
+            Console.WriteLine();
+            Console.WriteLine("1. add ");
+            Console.WriteLine("2. subtract");
+            Console.WriteLine("3. multiply");
+            Console.WriteLine("4. devide");
+            Console.WriteLine("5. Is the number odd or even?");
+
+
+            //you need a variable that you will use to store a number that the people chose like 1.2 .3 
+
+            int choice = Convert.ToInt16(Console.ReadLine());
+
+            //Create a switch to be able to pick an option(on what the user wants to doo, to call a method to calculate
+            
+        
+                Console.WriteLine(choice++);
+
+            switch (choice)
+            {
+
+                case 1:
+
+                    Console.WriteLine($"The sum of {Num1} and {Num2} is {Calculator.add(Num1, Num2)}");
+
+                    break;
+
+                case 2:
+
+                    Console.WriteLine($"The difference between {Num1} and {Num2} is {Calculator.subtract(Num1, Num2)}");
+                    break;
+
+                case 3:
+
+                    Console.WriteLine($"The product of {Num1} and {Num2} is {Calculator.multiply(Num1, Num2)}");
+                    break;
+
+                case 4:
+
+                    Console.WriteLine($"The quotient of {Num1} and {Num2} is {Calculator.devide(Num1, Num2)}");
+                    break;
+                case 5:
+                    Console.WriteLine($" {Calculator.iseven(Num1)}");
+                    Console.WriteLine($" {Calculator.iseven(Num2)}");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+
+                    break;
+
+
+            }
+        }
+       
+    }
+
+}
